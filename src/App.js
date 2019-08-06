66bbf58b97d79dcf519b676849e0717ae3d3bff5
Ifeoma <ifeomaokah@Ifeomas-MacBook-Pro.local>
2019-08-06 11:21:54 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Counter />
+      </div>
+    );
+  }
 }
 
+class Counter extends Component {
+  state = {
+    clickCount: 0
+  };
+
+  clickHandler = () => {
+    this.setState({
+      clickCount: this.state.clickCount + 1
+    });
+  };
+
+  render() {
+    return <button onClick={this.clickHandler}>{this.state.clickCount}</button>;
+  }
+}
 export default App;
